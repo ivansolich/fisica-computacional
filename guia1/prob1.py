480cd07249470f1a5064c9e58f7aca784ac7c6fb
ivans <ivansolich13@hotmail.com>
2024-08-30 01:11:37 -0300

    trash eliminate

--- guia1/prob1.py
+++ guia1/prob1.py
@@ -11,7 +11,7 @@ def derivada(x):
     return np.exp(x) * np.log(x) + np.exp(x) + 1 / x - 2 * x
 
 
-def Newton_Raphson(f, dx,x0, N, tol):
+def Newton_Raphson(f, dx, x0, N, tol):
     x = x0
     for i in range(N):
         x_new = x - f(x) / dx(x)
@@ -21,20 +21,20 @@ def Newton_Raphson(f, dx,x0, N, tol):
 
         x = x_new
 
+
 x = np.linspace(0.5, 2.5, 1000)
 
-raiz = Newton_Raphson(f, derivada, 2,100000,1e-9)
+raiz = Newton_Raphson(f, derivada, 2, 100000, 1e-9)
 print(raiz)
 
-raizSp = optimize.newton(f, 2,tol=1e-9)
+raizSp = optimize.newton(f, 2, tol=1e-9)
 print(raizSp)
-
 
 fig, ax = plt.subplots()
 
 plt.plot(x, f(x))
-plt.scatter(raiz,f(raiz),color='r')
-plt.scatter(raizSp,f(raizSp),color='green')
+plt.scatter(raiz, f(raiz), color='r')
+plt.scatter(raizSp, f(raizSp), color='green')
 plt.axhline(y=0, color='grey', linestyle='--', linewidth=1)
 
 plt.show()
