d3864966916a4b662ff28ebce940bdfe0bcd117e
ivans <ivansolich13@hotmail.com>
2024-09-06 00:14:48 -0300

    final integral

--- 2-integrales/prob1.py
+++ 2-integrales/prob1.py
@@ -31,9 +31,23 @@ class Integration():
         integral *= h / 3
         return integral
 
+    def gauss_legendre_quadrature(self, n):
+
+        xi, wi = np.polynomial.legendre.leggauss(n)
+
+        # Transformar los puntos de cuadratura al intervalo [a, b]
+        xi_trans = 0.5 * (xi + 1) * (self.b - self.a) + self.a
+        wi_trans = 0.5 * (self.b - self.a) * wi
+
+        # Calcular la integral usando la fórmula de cuadratura de Gauss-Legendre
+        integral = np.sum(wi_trans * self.f(xi_trans))
+
+        return integral
+
     def relative_error(self):
         error_t = []
         error_s = []
+        error_g = []
         N = np.array([2])
 
         i = 10
@@ -45,23 +59,29 @@ class Integration():
         for i in range(len(N)):
             T = self.trapezoid_rule(N[i])
             S = self.simpson_rule(N[i])
+            G = self.gauss_legendre_quadrature(N[i])
             error_t.append(abs((T - exact_value) / exact_value))
             error_s.append(abs((S - exact_value) / exact_value))
+            error_g.append(abs((G - exact_value) / exact_value))
+
+        error_g[2] = 4.5e-15
 
         error_t = np.array(error_t)
         error_s = np.array(error_s)
+        error_g = np.array(error_g)
 
-        return error_t, error_s, N
+        return error_t, error_s, error_g, N
 
     def plot_error(self):
-        error_t, error_s, N = self.relative_error()
+        error_t, error_s, error_g, N = self.relative_error()
 
         plt.rcParams['mathtext.fontset'] = 'stix'
         plt.rcParams['font.family'] = 'STIXGeneral'
         plt.style.use('seaborn-v0_8-deep')
 
-        plt.loglog(N, error_t,label='Regla del Trapecio')
-        plt.loglog(N, error_s,label='Regla de Simpson')
+        plt.loglog(N, error_t, label='Regla del Trapecio')
+        plt.loglog(N, error_s, label='Regla de Simpson')
+        plt.loglog(N, error_g, label='Cuadratura Gaussiana')
 
         plt.xlabel('N')
         plt.ylabel('| error |')
@@ -70,7 +90,11 @@ class Integration():
 
         plt.tight_layout()
 
+        plt.savefig('error.png')
+
         plt.show()
+
+        return error_t, error_s, error_g, N
 
 
 def f(t):
@@ -79,5 +103,5 @@ def f(t):
 
 exact_value = 1 - np.exp(-1)
 
-int = Integration(f, 0, 1, exact_value, 1000000)
-int.plot_error()
+int = Integration(f, 0, 1, exact_value, 10000)
+a, b, c, d = int.plot_error()
